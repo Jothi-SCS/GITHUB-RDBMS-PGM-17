43a88d3618dd49b3e0f5b01ab298df1fd8bdeaf9
Jothi-SCS <54353240+Jothi-SCS@users.noreply.github.com>
2026-09-22 13:45:42 +0530

    Update solution.sql

--- solution.sql
+++ solution.sql
@@ -1,12 +1,6 @@
--- PL/SQL Assignment
--- Write a PL/SQL program using FOR LOOP
--- to display numbers from 1 to 10.
-
-SET SERVEROUTPUT ON;
-
 BEGIN
-
-    -- Write your FOR LOOP here
-
+   FOR i IN 1..10 LOOP
+      DBMS_OUTPUT.PUT_LINE(i);
+   END LOOP;
 END;
 /
